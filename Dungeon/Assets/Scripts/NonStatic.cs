aa35024c51cb81345cc4cedc2383d8a1c550ab77
Millmoss <omelliff@yahoo.com>
2018-10-29 18:52:38 -0400

    Laying out more complex physics system

--- Dungeon/Assets/Scripts/NonStatic.cs
+++ Dungeon/Assets/Scripts/NonStatic.cs
@@ -7,32 +7,43 @@ public class NonStatic : MonoBehaviour
 	[SerializeField] protected Vector3 velocity = Vector3.zero;
 	[SerializeField] protected float gravityForce = 10f;
 	[SerializeField] protected float mass = 70f;
-	[SerializeField] protected float friction = .5f;
-	[SerializeField] protected float airResistance = .5f;
+	[SerializeField] protected float friction = .7f;
+	[SerializeField] protected float airResistance = .1f;
+	[SerializeField] protected float bounce = .1f;
 	protected Rigidbody selfBody;
+	protected Collider selfCollider;
 	private float positionDif = 0;
 	protected List<Collider> collisionList;
+	protected System.Type selfColliderType;
+	protected Vector3 force = Vector3.zero;
+	protected Vector3 forceReference = Vector3.zero;
 
 	void Start ()
 	{
 		collisionList = new List<Collider>();
 		selfBody = gameObject.GetComponent<Rigidbody>();
+		selfCollider = gameObject.GetComponent<Collider>();
+		selfColliderType = selfCollider.GetType();
 	}
 	
 	void Update ()
 	{
-		print(velocity);
+		
 	}
 
 	void FixedUpdate()
 	{
 		stabilize();
 
-		//do some fancy maths to figure out how solidly the object is on the ground
+		force = force - forceReference;
 
-		velocity += gravityForce * Vector3.down * Time.deltaTime;
+		force += gravityForce * Vector3.down;
 
-		transform.position += velocity * Time.deltaTime;
+		generalCollisionHandler();
+
+		velocityHandler();
+
+		forceReference = force;
 	}
 
 	void LateUpdate()
@@ -45,15 +56,68 @@ public class NonStatic : MonoBehaviour
 		selfBody.velocity = Vector3.zero;
 	}
 
-	public void OnCollisionEnter(Collision c)
+	protected void generalCollisionHandler()
 	{
-		if (c.gameObject.layer == 10)
+		if (selfColliderType == typeof(SphereCollider))
+		{
+			bool onGround = false;
+
+			for (int i = 0; i < collisionList.Count; i++)
+			{
+				Collider c = collisionList[i];
+
+				//get force from vector using closest point on object
+				//if close enough to a grounding force, treat as though it is the ground
+				//use velocity in moment and the friction of the two objects to determine rotation force
+
+				if (c.gameObject.layer == 11 || c.gameObject.layer == 12)
+				{
+					//send force to object
+				}
+			}
+
+			//if on ground and upward force greater than gravity is non-existant, gravity should not apply
+		}
+		if (selfColliderType == typeof(BoxCollider))
+		{
+			float percentOnGround = 0;
+
+			for (int i = 0; i < collisionList.Count; i++)
+			{
+				Collider c = collisionList[i];
+
+				//get force from closest point against closest point to find rotation force
+				//if close enough to a grounding force, treat as though it is the ground
+
+				if (c.gameObject.layer == 11 || c.gameObject.layer == 12)
+				{
+					//send force to object
+				}
+			}
+
+			//use percent on ground to figure out friction, whether gravity should even apply, and so on
+		}
+	}
+
+	protected void velocityHandler()
+	{
+
+	}
+
+	public void addForce(Vector3 f)
+	{
+		force += f;
+	}
+
+	void OnCollisionEnter(Collision c)
+	{
+		if (c.gameObject.layer == 10 || c.gameObject.layer == 11 || c.gameObject.layer == 12)
 			collisionList.Add(c.collider);
 	}
 
-	public void OnCollisionExit(Collision c)
+	void OnCollisionExit(Collision c)
 	{
-		if (c.gameObject.layer == 10)
+		if (c.gameObject.layer == 10 || c.gameObject.layer == 11 || c.gameObject.layer == 12)
 			collisionList.Remove(c.collider);
 	}
 }
