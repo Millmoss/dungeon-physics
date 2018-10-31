f4ef9c5c7f98001e8109675ff3edd91f2a6652c1
Millmoss <omelliff@yahoo.com>
2018-10-30 23:53:20 -0400

    Ball can bounce wow

--- Dungeon/Assets/Scripts/NonStatic.cs
+++ Dungeon/Assets/Scripts/NonStatic.cs
@@ -6,24 +6,32 @@ public class NonStatic : MonoBehaviour
 {
 	[SerializeField] protected Vector3 velocity = Vector3.zero;
 	[SerializeField] protected float gravityForce = 10f;
-	[SerializeField] protected float mass = 70f;
-	[SerializeField] protected float friction = .7f;
-	[SerializeField] protected float airResistance = .1f;
-	[SerializeField] protected float bounce = .1f;
+	[SerializeField] protected float mass = 5f;
+	[SerializeField] [Range(0f, 1f)] protected float friction = .7f;
+	[SerializeField] [Range(0f, 1f)] protected float airResistance = .1f;
+	[SerializeField] [Range(0f, 1f)] protected float bounce = .1f;
+	[SerializeField] [Range(-1f, 1f)] protected float adhesion = 0f;		//higher than zero to make more adhesive, lower than zero to make preventative of adhesion. Most objects should be zero
 	protected Rigidbody selfBody;
 	protected Collider selfCollider;
-	private float positionDif = 0;
 	protected List<Collider> collisionList;
 	protected System.Type selfColliderType;
 	protected Vector3 force = Vector3.zero;
 	protected Vector3 forceReference = Vector3.zero;
+	protected float extent = 0;
+	protected LayerMask collisionMask;
 
 	void Start ()
 	{
 		collisionList = new List<Collider>();
 		selfBody = gameObject.GetComponent<Rigidbody>();
+		selfBody.freezeRotation = true;
 		selfCollider = gameObject.GetComponent<Collider>();
 		selfColliderType = selfCollider.GetType();
+
+		extent = Mathf.Sqrt(Mathf.Max(selfCollider.bounds.extents.x, selfCollider.bounds.extents.y, selfCollider.bounds.extents.z)
+			+ Mathf.Max(selfCollider.bounds.extents.x, selfCollider.bounds.extents.y, selfCollider.bounds.extents.z));
+
+		collisionMask = (LayerMask)111 << 9;
 	}
 	
 	void Update ()
@@ -37,11 +45,15 @@ public class NonStatic : MonoBehaviour
 
 		force = force - forceReference;
 
-		force += gravityForce * Vector3.down;
+		force += gravityForce * mass * Vector3.down;
 
 		generalCollisionHandler();
 
 		velocityHandler();
+
+		print(velocity);
+
+		transform.position += velocity * Time.deltaTime;
 
 		forceReference = force;
 	}
@@ -58,13 +70,21 @@ public class NonStatic : MonoBehaviour
 
 	protected void generalCollisionHandler()
 	{
+		if (collisionList.Count == 0)
+			return;
+
 		if (selfColliderType == typeof(SphereCollider))
 		{
-			bool onGround = false;
+			Vector3 groundingNormal = Vector3.zero;
 
 			for (int i = 0; i < collisionList.Count; i++)
 			{
 				Collider c = collisionList[i];
+
+				Vector3 normal = (transform.position - c.ClosestPointOnBounds(transform.position)).normalized;
+
+				if (Vector3.Angle(normal, Vector3.up) < 90)
+					groundingNormal += normal;
 
 				//get force from vector using closest point on object
 				//if close enough to a grounding force, treat as though it is the ground
@@ -73,6 +93,19 @@ public class NonStatic : MonoBehaviour
 				if (c.gameObject.layer == 11 || c.gameObject.layer == 12)
 				{
 					//send force to object
+				}
+			}
+
+			if (Vector3.Angle(groundingNormal, Vector3.up) < 1 && groundingNormal != Vector3.zero)
+			{
+				force += gravityForce * mass * Vector3.up;
+				if (velocity.y < .05f)
+				{
+					velocity += Vector3.up * -velocity.y * bounce + Vector3.up * -velocity.y;
+				}
+				else if(velocity.y < 0)
+				{
+					velocity += Vector3.up * -velocity.y;
 				}
 			}
 
@@ -85,6 +118,9 @@ public class NonStatic : MonoBehaviour
 			for (int i = 0; i < collisionList.Count; i++)
 			{
 				Collider c = collisionList[i];
+
+				RaycastHit hit = new RaycastHit();
+				bool ray = Physics.Raycast(transform.position, Vector3.down, extent, 
 
 				//get force from closest point against closest point to find rotation force
 				//if close enough to a grounding force, treat as though it is the ground
@@ -101,7 +137,7 @@ public class NonStatic : MonoBehaviour
 
 	protected void velocityHandler()
 	{
-
+		velocity += (force / mass) * Time.deltaTime;
 	}
 
 	public void addForce(Vector3 f)
